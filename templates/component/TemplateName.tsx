fc9150d2214f74e2dde09d4557bf65f7fbbc7048
TheBaconStyle <maks.christmas2001@gmail.com>
2022-10-22 00:39:51 +0200

    updated template files

--- templates/component/TemplateName.tsx
+++ templates/component/TemplateName.tsx
@@ -1,6 +1,7 @@
+import { BaseComponentProps } from 'src/shared/types/component.type'
 import styles from './TemplateName.module.scss'
 
-export interface TemplateNameProps {}
+export interface TemplateNameProps extends BaseComponentProps {}
 
 export function TemplateName({}: TemplateNameProps) {
   return <div className={styles.templateName}>TemplateName component</div>
--- templates/page/index.tsx
+++ templates/page/index.tsx
@@ -1,21 +1,17 @@
-import { GetServerSideProps } from 'next'
 import Head from 'next/head'
-import { Layout } from 'src/client/components/Layout'
-import { BasePageProps } from 'src/shared/types/page.type'
-import { gssp } from 'src/shared/utils/gssp.util'
-import styles from './page.module.scss'
+import { Header } from 'src/client/components/Header'
+import styles from './templatename.module.scss'
 
-export const getServerSideProps: GetServerSideProps = gssp
-
-export interface TemplateNamePageProps extends BasePageProps {}
+export interface TemplateNamePageProps {}
 
 export default function TemplateNamePage({}: TemplateNamePageProps) {
   return (
-    <Layout>
+    <>
+      <Header items={[]} />
       <Head>
         <title>TemplateName page</title>
       </Head>
       <div className={styles.templateNamePage}>TemplateName page</div>
-    </Layout>
+    </>
   )
 }
