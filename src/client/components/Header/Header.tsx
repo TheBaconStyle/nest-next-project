3dd18fa8d02761c386019346d9efbd2067ce5793
TheBaconStyle <maks.christmas2001@gmail.com>
2022-10-28 21:00:48 +0200

    updated client header component

--- src/client/components/Header/Header.tsx
+++ src/client/components/Header/Header.tsx
@@ -1,18 +1,23 @@
-import { AnimatePresence, motion } from 'framer-motion'
-import React, { useEffect } from 'react'
-import { FiMenu, FiUser, FiUserX, FiX } from 'react-icons/fi'
-import { useBoolean, useIsClient, useMediaQuery } from 'usehooks-ts'
-import { authorizedRoots } from './authorizedRoots'
-import { unauthorizedRoots } from './unauthorizedRoots'
-import { AnchorLink } from '../AnchorLink'
-import { Progress } from '../Progress'
+import {AnimatePresence, motion} from 'framer-motion'
+import {ReactNode, useEffect} from 'react'
+import {IconType} from 'react-icons'
+import {FiMenu, FiX} from 'react-icons/fi'
+import {useBoolean, useIsClient, useMediaQuery} from 'usehooks-ts'
+import {AnchorLink} from '../AnchorLink'
+import {Progress} from '../Progress'
 import styles from './Header.module.scss'
 
-export interface HeaderProps {
-  user: string
+export interface MenuItem {
+  href: string
+  label: ReactNode
+  icon: IconType
 }
 
-export function Header({ user }: HeaderProps) {
+interface HeaderProps {
+  items?: MenuItem[]
+}
+
+export function Header({items}: HeaderProps) {
   const {
     toggle: toggleMenu,
     setFalse: hideMenu,
@@ -23,109 +28,66 @@ export function Header({ user }: HeaderProps) {
   useEffect(() => {
     hideMenu()
   }, [isDesktop, hideMenu])
-  const handleMenuButtonClick = () => {
-    toggleMenu()
-  }
   return (
     <header className={styles.navbar}>
-      <Progress />
+      <Progress/>
       <nav className={styles.nav}>
         <AnchorLink className={styles.navbar_brand} href="/">
           Brand
         </AnchorLink>
-        {isClient && !isDesktop && (
+        {items && items.length !== 0 && !isDesktop && isClient && (
           <motion.button
             className={styles.navbar_collapse_toggler}
-            onClick={handleMenuButtonClick}
-            whileTap={{ scale: 0.95 }}
+            onClick={toggleMenu}
+            whileTap={{scale: 0.95}}
           >
             <AnimatePresence exitBeforeEnter initial={false}>
-              {isMenuVisible ? (
-                <motion.div
-                  initial={{
-                    scale: 1.5,
-                    opacity: 0,
-                  }}
-                  animate={{
-                    scale: 1,
-                    opacity: 1,
-                  }}
-                  exit={{ scale: 0.5, opacity: 0 }}
-                  key={'close'}
-                >
-                  <FiX className={styles.navbar_collapse_toggler_icon} />
-                </motion.div>
-              ) : (
-                <motion.div
-                  initial={{
-                    scale: 1.5,
-                    opacity: 0,
-                  }}
-                  animate={{
-                    scale: 1,
-                    opacity: 1,
-                  }}
-                  exit={{ scale: 0.5, opacity: 0 }}
-                  key={'open'}
-                >
-                  <FiMenu className={styles.navbar_collapse_toggler_icon} />
-                </motion.div>
-              )}
+              <motion.div
+                initial={{
+                  scale: 1.5,
+                  opacity: 0,
+                }}
+                animate={{
+                  scale: 1,
+                  opacity: 1,
+                }}
+                exit={{scale: 0.5, opacity: 0}}
+                key={isMenuVisible ? 'OPEN' : 'CLOSE'}
+              >
+                {isMenuVisible ? (
+                  <FiX className={styles.navbar_collapse_toggler_icon}/>
+                ) : (
+                  <FiMenu className={styles.navbar_collapse_toggler_icon}/>
+                )}
+              </motion.div>
             </AnimatePresence>
           </motion.button>
         )}
-        <motion.div
-          initial={{ height: isDesktop || isMenuVisible ? '100%' : '0' }}
-          className={styles.navbar_collapse}
-          animate={{ height: isDesktop || isMenuVisible ? '100%' : '0' }}
-        >
-          <div className={styles.navbar_collapse_item_wrapper}>
-            {!user && (
-              <>
-                {unauthorizedRoots.map((root) => (
-                  <AnchorLink
-                    className={styles.navbar_collapse_item}
-                    activeClass={styles.active}
-                    href={root.href}
-                    key={root.href}
-                  >
-                    {root.label}
-                  </AnchorLink>
-                ))}
-              </>
-            )}
-            {user && (
-              <>
-                {authorizedRoots.map((root) => (
-                  <AnchorLink
-                    className={styles.navbar_collapse_item}
-                    activeClass={styles.active}
-                    href={root.href}
-                    key={root.href}
-                  >
-                    {root.label}
-                  </AnchorLink>
-                ))}
+        {isClient && (
+          <motion.div
+            initial={{
+              height: isDesktop || isMenuVisible ? '100%' : '0',
+            }}
+            className={styles.navbar_collapse}
+            animate={{
+              height: isDesktop || isMenuVisible ? '100%' : '0',
+            }}
+          >
+            <div className={styles.navbar_collapse_item_wrapper}>
+              {items?.map(({href, icon: Icon, label}) => (
                 <AnchorLink
+                  href={href}
                   className={styles.navbar_collapse_item}
-                  activeClass={styles.active}
-                  href="/user/profile"
+                  key={href}
+                  activeClass={styles.item_active}
                 >
-                  <FiUser className={styles.navbar_collapse_item_icon} />
-                  Профиль: <strong>{user}</strong>
+                  <Icon className={styles.navbar_collapse_item_icon}/>
+                  {label}
                 </AnchorLink>
-                <AnchorLink
-                  className={styles.navbar_collapse_item}
-                  activeClass={styles.active}
-                  href="/auth/api/signout"
-                >
-                  <FiUserX className={styles.navbar_collapse_item_icon} />
-                  Выйти
-                </AnchorLink>
-              </>
-            )}
-          </div>
-        </motion.div>
+              ))}
+            </div>
+          </motion.div>
+        )}
       </nav>
     </header>
   )
