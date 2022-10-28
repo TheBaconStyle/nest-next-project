957b394c8ac18e15fdfd9e64986baa99ee527ed8
TheBaconStyle <maks.christmas2001@gmail.com>
2022-10-28 21:05:30 +0200

    updated next _app component & page structure

--- src/pages/views/error/index.tsx
+++ src/pages/views/error/index.tsx
@@ -1,17 +1,14 @@
 import Error from 'next/error'
 import {gssp} from 'src/shared/utils/gssp.util'
-
-export const getServerSideProps = gssp
+import {Header} from "../../../client/components/Header";
 
 export interface ErrorPageProps {
   status: number
   message: string
 }
 
+export const getServerSideProps = gssp
+
 export default function ErrorPage({status, message}: ErrorPageProps) {
-  return (
-    <>
-      <Error statusCode={status} title={message}/>
-    </>
-  )
+  return <><Header/><Error statusCode={status} title={message}/></>
 }
--- src/pages/views/home/index.tsx
+++ src/pages/views/home/index.tsx
@@ -1,17 +1,49 @@
 import Head from 'next/head'
-import { Layout } from 'src/client/components/Layout'
-import { BasePageProps } from 'src/shared/types/page.type'
-import { gssp } from 'src/shared/utils/gssp.util'
+import {Header} from 'src/client/components/Header'
+import {FaHome, FaUserCheck, FaUserLock, FaUserPlus, FaUserTimes} from 'react-icons/fa'
+import {gssp} from 'src/shared/utils/gssp.util'
+import {Footer} from "../../../client/components/Footer";
+import {Slider} from "../../../client/components/Slider";
+import {FormInput} from "../../../client/components/FormInput";
+import {Calendar} from "../../../client/components/Calendar";
+import {Button} from "../../../client/components/Button";
 
 export const getServerSideProps = gssp
 
-export default function Home({ user }: BasePageProps) {
+interface HomePageProps {
+  user: string
+}
+
+export default function Home({user}: HomePageProps) {
   return (
-    <Layout user={user}>
+    <>
+      <Header
+        items={
+          !user
+            ? [
+              {href: '/signin', label: 'Войти', icon: FaUserCheck},
+              {href: '/signup', label: 'Регистрация', icon: FaUserPlus},
+            ]
+            : [
+              {href: '/', label: 'Домашняя', icon: FaHome},
+              {
+                href: '/account',
+                label: `Личный кабинет: ${user}`,
+                icon: FaUserLock,
+              },
+
+              {href: '/signout', label: 'Выйти', icon: FaUserTimes},
+            ]
+        }
+      />
       <Head>
         <title>OSL: Главная</title>
       </Head>
-      home
-    </Layout>
+      <FormInput/>
+      <Button/>
+      <Calendar/>
+      <Slider/>
+      <Footer/>
+    </>
   )
 }
