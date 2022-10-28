67b818e7332a4f7dc0c1d5ad8dfc4ee2a6794be0
TheBaconStyle <maks.christmas2001@gmail.com>
2022-10-28 21:06:27 +0200

    updated http exception filter

--- src/server/filters/basic.filter.ts
+++ src/server/filters/basic.filter.ts
@@ -1,25 +1,20 @@
-import {
-  ArgumentsHost,
-  Catch,
-  ExceptionFilter,
-  HttpException,
-} from '@nestjs/common'
-import { Request, Response } from 'express'
-import { RenderableResponse } from 'nest-next'
+import {ArgumentsHost, Catch, ExceptionFilter, HttpException,} from '@nestjs/common'
+import {Request, Response} from 'express'
+import {RenderableResponse} from 'nest-next'
 
 @Catch(HttpException)
 export class BasicFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
-    if (process.env.NODE_ENV === 'development') console.log(exception)
     const ctx = host.switchToHttp()
+    const request = ctx.getRequest<Request>()
     const status = exception.getStatus()
     const message = exception.message
-    const response = ctx.getResponse<Response & RenderableResponse>()
-    const request = ctx.getRequest() as Request
+    const response = ctx.getResponse<RenderableResponse & Response>()
+    // if (process.env.NODE_ENV === 'development') console.log(exception)
     response.status(status)
-    if (request.method === 'GET') {
-      return response.render(`errors/${status}`)
+    if (request.path.includes('api')) {
+      return response.json({message})
     }
-    response.json({ message })
+    response.render('error', {status, message})
   }
 }
