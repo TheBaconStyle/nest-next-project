d5dadd7f6684767ff5d4febaac4632740c627a50
TheBaconStyle <maks.christmas2001@gmail.com>
2022-10-28 20:55:02 +0200

    updated protect docs middleware

--- src/server/middlewares/protect-docs.middleware.ts
+++ src/server/middlewares/protect-docs.middleware.ts
@@ -1,8 +1,11 @@
-import { Injectable, NestMiddleware } from '@nestjs/common'
+import {ForbiddenException, Injectable, NestMiddleware} from '@nestjs/common'
+import {Request} from 'express'
+import {havePermissions} from '../auth/guards/permission.guard'
 
 @Injectable()
 export class ProtectDocsMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
-    next()
+  async use(req: Request, _res: any, next: () => void) {
+    if (await havePermissions(req.user, ['haveDocsAccess'])) return next()
+    throw new ForbiddenException()
   }
 }
