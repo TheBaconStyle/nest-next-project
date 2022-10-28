f43f7fc7ad8dabdc5ab41ebd0aae7c597bcb8566
TheBaconStyle <maks.christmas2001@gmail.com>
2022-10-28 21:06:09 +0200

    updated app controller

--- src/server/app.controller.ts
+++ src/server/app.controller.ts
@@ -1,16 +1,51 @@
-import { Controller, Get, Render } from '@nestjs/common'
-import { ApiExcludeController } from '@nestjs/swagger'
-import { CurrentUser } from './decorators/request-user.decorator'
-import { User } from './entities/users.entity'
+import {Controller, Get, NotFoundException, Redirect, Render, Session, UseGuards,} from '@nestjs/common'
+import {ApiExcludeController} from '@nestjs/swagger'
+import {CurrentUser} from "./decorators/request-user.decorator";
+import {User} from "./entities";
+import {UnauthorizedGuard} from "./auth/guards/unauthorized.guard";
+import {AuthorizeGuard} from "./auth/guards/authorize.guard";
 
 @ApiExcludeController()
 @Controller()
 export class AppController {
+  constructor() {
+  }
+
   @Render('home')
   @Get()
-  getHello(@CurrentUser() user: User) {
-    return {
-      user: user ? user.login : null,
-    }
+  home(@CurrentUser() user: User) {
+    if (user) return {user: user.login,}
+  }
+
+  @UseGuards(UnauthorizedGuard)
+  @Render('signin')
+  @Get('signin')
+  @Redirect('/', 200)
+  signin() {
+  }
+
+  @UseGuards(UnauthorizedGuard)
+  @Render('signup')
+  @Get('signup')
+  @Redirect('/', 200)
+  signup() {
+  }
+
+  @UseGuards(AuthorizeGuard)
+  @Render('account')
+  @Get('account')
+  account(@CurrentUser() user: User) {
+    if (user) return {user: user.login,}
+  }
+
+  @Get('signout')
+  @Redirect('/')
+  async signout(@Session() session: any) {
+    if (session) await session.destroy()
+  }
+
+  @Get('qwe')
+  async qwe() {
+    throw new NotFoundException()
   }
 }
