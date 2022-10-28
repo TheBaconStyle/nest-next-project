92b779b8fe03f1e62be66f06e06eb153ad817a99
TheBaconStyle <maks.christmas2001@gmail.com>
2022-10-28 21:01:11 +0200

    updated client preloader component

--- src/client/components/Preloader/Preloader.tsx
+++ src/client/components/Preloader/Preloader.tsx
@@ -1,17 +1,17 @@
-import styles from './preloader.module.scss'
+import styles from './Preloader.module.scss'
 
-import { motion } from 'framer-motion'
-import { AiOutlineLoading3Quarters } from 'react-icons/ai'
+import {motion} from 'framer-motion'
+import {AiOutlineLoading3Quarters} from 'react-icons/ai'
 
 export function Preloader() {
   return (
     <motion.div
       className={styles.preloader}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
+      initial={{opacity: 0}}
+      animate={{opacity: 1}}
+      exit={{opacity: 0}}
     >
-      <AiOutlineLoading3Quarters className={styles.preloader_icon} />
+      <AiOutlineLoading3Quarters className={styles.preloader_icon}/>
     </motion.div>
   )
 }
