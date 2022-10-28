55b861d69baa5f86d7ac88487f2147fca3e8ce3b
TheBaconStyle <maks.christmas2001@gmail.com>
2022-10-28 21:02:40 +0200

    updated client progress component

--- src/client/components/Progress/Progress.tsx
+++ src/client/components/Progress/Progress.tsx
@@ -1,12 +1,12 @@
-import { motion, useScroll } from 'framer-motion'
+import {motion, useScroll} from 'framer-motion'
 import styles from './Progress.module.scss'
 
 export function Progress() {
-  const { scrollYProgress } = useScroll()
+  const {scrollYProgress: scaleX} = useScroll()
   return (
     <motion.div
       className={styles.progress}
-      style={{ scaleX: scrollYProgress }}
-    ></motion.div>
+      style={{scaleX}}
+    />
   )
 }
