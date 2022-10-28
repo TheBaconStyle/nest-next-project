627935dc93ff1aadf6e9ac1982b375bc4b6a9547
TheBaconStyle <maks.christmas2001@gmail.com>
2022-10-28 21:00:29 +0200

    added client footer component

---
+++ src/client/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import styles from './Footer.module.scss'
+
+export interface FooterProps {
+}
+
+export function Footer({}: FooterProps) {
+  return <div className={styles.footer}>my first nest-next project</div>
+}
